41a8e3641449add989dc2f81d75fa040ee327272
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-03-11 10:01:38 -0700

    updated comments for application.js

--- app/application.js
+++ app/application.js
@@ -5,5 +5,26 @@ outline:
 Deck class
 Card class
 Hand class
+
+Game Rules:
+card values 	- respective numeric values
+				- face cards = 10
+				- Ace = 1 or 11
+
+Player hand: 	- hit/stand
+				- hand value > 21 is a bust
+				- no restrictions on standing
+
+Dealer hand: 	- Begins turn after player stands
+				- hit/stand
+				- must hit if hand value below <17
+				- in this casino, must hit on soft 17 (17 with ace)
+
+Why "basic" blackjack (could be future implementation)
+- no ability to double down
+- no split
+- no betting
+- one deck in play
+
 */
 
